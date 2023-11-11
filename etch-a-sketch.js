80ece38e8b04137e93db03654c2c5f3960090725
YagoDanti <yago.ladeiadanti@gmail.com>
2023-11-11 13:35:25 -0800

    js update to attribute values to the buttons

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,46 @@
-const mainContainer = document.createElement('div'); // create a div element
-mainContainer.classList.add('mainContainer'); // add a class to the div element
+const mainContainer = document.createElement('div');
+mainContainer.classList.add('mainContainer');
 
-const numberOfGrids = 16; // number of total grids
+const numberOfGrids = 256;
 
 for (let i = 0; i < numberOfGrids; i++) {
     const gridMainContainer = document.createElement('div');
     gridMainContainer.classList.add('gridMainContainer');
     mainContainer.appendChild(gridMainContainer);
-} // loop to create 16 div with class gridMainContainer and append it to mainContainer
+}
 
-const mainContainerElement = document.getElementById('body'); // select the body
-mainContainerElement.appendChild(mainContainer); // append the div inside the body
+const mainContainerElement = document.getElementById('bigContainer');
+mainContainerElement.appendChild(mainContainer);
+
+//
+
+const btnRed = document.getElementsByClassName('btnRed')[0];
+const btnGreen = document.getElementsByClassName('btnGreen')[0];
+const btnBlue = document.getElementsByClassName('btnBlue')[0];
+
+const gridContainers = document.querySelectorAll('.gridMainContainer');
+
+btnRed.addEventListener('click', () => {
+    gridContainers.forEach((gridContainer) => {
+        gridContainer.addEventListener('click', () => {
+            gridContainer.style.backgroundColor = 'red';
+        });
+    });
+})
+
+btnGreen.addEventListener('click', () => {
+    gridContainers.forEach((gridContainer) => {
+        gridContainer.addEventListener('click', () => {
+            gridContainer.style.backgroundColor = 'green';
+        });
+    });
+});
+
+btnBlue.addEventListener('click', () => {
+    gridContainers.forEach((gridContainer) => {
+        gridContainer.addEventListener('click', () => {
+            gridContainer.style.backgroundColor = 'blue';
+        });
+    });   
+})
 
