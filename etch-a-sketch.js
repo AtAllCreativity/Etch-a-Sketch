004275434f853c56206fdaa5c7930a5d2d4d9b89
YagoDanti <yago.ladeiadanti@gmail.com>
2023-11-14 15:27:09 -0800

    Create function for color. When color is selected it will be shown a list with all colors to select from

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,8 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     let btnDark = document.querySelector('.btn-dark');
     let btnDanger = document.querySelector('.btn-danger');
     let btnSuccess = document.querySelector('.btn-success');
-    let gridItems = document.querySelectorAll('.grid-items')
+    let btnPrimary = document.querySelector('.btn-primary');
+    let gridItems = document.querySelectorAll('.grid-items');
     let container = document.querySelector('.container');
+    let colorPicker = document.getElementById('color-picker');
 
     btnDark.addEventListener('click', function() {
         container.addEventListener('click', function(event) {
@@ -24,6 +26,17 @@ document.addEventListener('DOMContentLoaded', function() {
             handleGridItemClick(event, getRandomColor());
         });
     });
+
+    btnPrimary.addEventListener('click', function() {
+        showColorPicker(colorPicker);
+    });
+
+    colorPicker.addEventListener('input', function() {
+        container.addEventListener('click', function(event) {
+            handleGridItemClick(event, colorPicker.value);
+        });
+    });
+
 });
 
 function createBoard(numberOfGrids) {
@@ -36,13 +49,17 @@ function createBoard(numberOfGrids) {
     }
 }
 
-function getRandomColor() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
 function handleGridItemClick(event, color) {
     const clickedElement = event.target;
     if (clickedElement.classList.contains('grid-items')) {
         clickedElement.style.backgroundColor = color;
     }
+}
+
+function showColorPicker(colorPicker) {
+    colorPicker.click();
+}
+
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
